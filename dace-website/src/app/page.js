9a89b7fe67c3367013812fff9e6521928720781a
varuncsed1 <36481529+varuncsed1@users.noreply.github.com>
2025-07-17 12:35:32 +0530

    Update page.js

--- dace-website/src/app/page.js
+++ dace-website/src/app/page.js
@@ -43,6 +43,49 @@ export default function Home() {
           H.N.B. Garhwal University
         </h2>
       </header>
+
+            <h2 className={styles.heading}>Vice Chancellor</h2>
+      <div className={styles.grid}>
+        {/* Faculty 1 */}
+        <div className={styles.card}>
+          <Image
+            src="/Picture10.jpeg"
+            alt="Dr. Shri Prakash"
+            width={400}
+            height={400}
+            className={styles.image}
+          />
+          <h3 className={styles.name}>Dr. Shri Prakash</h3>
+          <p className={styles.details}>
+            (Social Sciences)<br />
+            (Joined on 7 Nov. 2022)<br />
+            M.A. Geography<br />
+            NET-JRF Qualified<br />
+            03 times UPSC mains exam appeared<br />
+            Multiple State Services Exams Qualified
+          </p>
+        </div>
+
+        {/* Faculty 2 */}
+        <div className={styles.card}>
+          <Image
+            src="/Picture11.png"
+            alt="Dr. Ashish Bahuguna"
+            width={400}
+            height={400}
+            className={styles.image}
+          />
+          <h3 className={styles.name}>Dr. Ashish Bahuguna</h3>
+          <p className={styles.details}>
+            (Science &amp; Technology)<br />
+            (Joined on 10 Nov. 2022)<br />
+            M.Sc. Chemistry<br />
+            NET-JRF, GATE, U-SET Qualified<br />
+            PhD from IIT Mandi<br />
+            Postdoc (Israel)<br />
+            02 Times UPSC-CDS Qualified
+          </p>
+        </div>
       <Vision />
       <section id="mission" style={{ scrollMarginTop: "80px", padding: "3rem 1rem", maxWidth: "64rem", margin: "0 auto" }}>
         <Mission/>
